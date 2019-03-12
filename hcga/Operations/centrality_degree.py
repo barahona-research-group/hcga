76b69df3ca937352993fb64e9e9561fe9cd6dc71
Robert Peach <robertlucienpeach@gmail.com>
2019-03-12 13:41:50 +0000

    separate features are now classes

--- hcga/Operations/centrality_degree.py
+++ hcga/Operations/centrality_degree.py
@@ -1,18 +1,55 @@
 from networkx.algorithms import centrality
-from hcga.Operations import DistributionFitting
+from hcga.Operations import utils
 import numpy as np
 
+
+
+class DegreeCentrality():
+    def __init__(self, G):
+        self.G = G
+        self.feature_names = []
+        self.features = []
+
+    def feature_extraction(self,bins=10):
+        """
+        Input: bins - the number of bins for the calculating the SSE of fits. Default = 10 bins
+
+        """
+
+        self.feature_names = ['mean','std','opt_model','powerlaw_a','powerlaw_SSE']
+
+        G = self.G
+
+        feature_list = []
+
+        #Calculate the degree centrality of each node
+        degree_centrality = np.asarray(list(centrality.degree_centrality(G).values()))
+
+        # Basic stats regarding the degree centrality distribution
+        feature_list.append(degree_centrality.mean())
+        feature_list.append(degree_centrality.std())
+
+        # Fitting the degree centrality distribution and finding the optimal
+        # distribution according to SSE
+        opt_mod,opt_mod_sse = utils.best_fit_distribution(degree_centrality,bins=bins)
+        feature_list.append(opt_mod)
+
+        # Fitting power law and finding 'a' and the SSE of fit.
+        feature_list.append(utils.power_law_fit(degree_centrality,bins=bins)[0][-2]) # value 'a' in power law
+        feature_list.append(utils.power_law_fit(degree_centrality,bins=bins)[1]) # value sse in power law
+
+        # Fitting normal distribution and finding...
+
+
+        # Fitting exponential and finding ...
+
+
+        self.features = feature_list
+
+
+"""
 def DegreeCentrality(G,bins):
-    """
-    Input: Graph networkx
 
-    Output: Dictionary of heuristic graph stats
-
-    Stats:
-
-
-
-    """
     feature_names = ['mean','std','opt_model','powerlaw_a','powerlaw_SSE']
     feature_list = []
 
@@ -40,3 +77,4 @@ def DegreeCentrality(G,bins):
 
 
     return (feature_names,feature_list)
+"""
--- hcga/Operations/triangles.py
+++ hcga/Operations/triangles.py
@@ -9,20 +9,48 @@ import pandas as pd
 import numpy as np
 import networkx as nx
 
+class Triangles():
+    def __init__(self, G):
+        self.G = G
+        self.feature_names = []
+        self.features = []
 
+    def feature_extraction(self):
+
+        self.feature_names = ['num_triangles','transitivity','clustering_mean',
+                        'clustering_std','clustering_median','square_clustering_mean',
+                        'square_clustering_std','square_clustering_median']
+
+        G = self.G
+
+        feature_list = []
+
+        # Calculating number of triangles
+        feature_list.append(np.asarray(list(nx.triangles(G).values())).mean())
+
+        # graph transivity
+        feature_list.append(nx.transitivity(G))
+
+        # Average clustering coefficient
+        feature_list.append(nx.average_clustering(G))
+        feature_list.append(np.asarray(list(nx.clustering(G).values())).std())
+        feature_list.append(np.median(np.asarray(list(nx.clustering(G).values()))))
+
+        # generalised degree
+        feature_list.append(np.asarray(list(nx.square_clustering(G).values())).mean())
+        feature_list.append(np.asarray(list(nx.square_clustering(G).values())).std())
+        feature_list.append(np.median(np.asarray(list(nx.square_clustering(G).values()))))
+
+
+        self.features = feature_list
+
+
+
+"""
 def triangle_stats(G):
-    """
-    Input: Graph networkx
 
-    Output: Dictionary of clustering stats
-
-    Stats:
-
-
-
-    """
     feature_names = ['num_triangles','transitivity','clustering_mean',
-                    'clustering_std','clustering_median','square_clustering_mean'
+                    'clustering_std','clustering_median','square_clustering_mean',
                     'square_clustering_std','square_clustering_median']
     feature_list = []
 
@@ -43,3 +71,4 @@ def triangle_stats(G):
     feature_list.append(np.median(np.asarray(list(nx.square_clustering(G).values()))))
 
     return (feature_names,feature_list)
+"""
