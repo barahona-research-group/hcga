cd470575988e217e58320675fb867eba61b4b751
Henry Palasciano <Henry@dyn3131-31.wlan.ic.ac.uk>
2019-07-19 14:27:39 +0100

    moved bins and made changes to edge betweenness centrality

--- hcga/Operations/centrality_edge_betweenness.py
+++ hcga/Operations/centrality_edge_betweenness.py
@@ -15,7 +15,7 @@ class EdgeBetweennessCentrality():
         self.feature_names = []
         self.features = []
 
-    def feature_extraction(self,args):
+    def feature_extraction(self):
         """
         Compute the edge betweenness centrality for nodes.
         
@@ -45,10 +45,10 @@ class EdgeBetweennessCentrality():
         """     
                 
         # Defining the input arguments
-        bins = args[0]
+        bins = [10,20,50]
 
         # Defining featurenames
-        feature_names = ['mean','std','opt_model','powerlaw_a','powerlaw_SSE']
+        feature_names = ['mean','std','max','min']
 
         G = self.G
 
@@ -60,15 +60,24 @@ class EdgeBetweennessCentrality():
         # Basic stats regarding the edge betweenness centrality distribution
         feature_list.append(edge_betweenness_centrality.mean())
         feature_list.append(edge_betweenness_centrality.std())
+        feature_list.append(edge_betweenness_centrality.max())
+        feature_list.append(edge_betweenness_centrality.min())
 
-        # Fitting the edge betweenness centrality distribution and finding the optimal
-        # distribution according to SSE
-        opt_mod,opt_mod_sse = utils.best_fit_distribution(edge_betweenness_centrality,bins=bins)
-        feature_list.append(opt_mod)
 
-        # Fitting power law and finding 'a' and the SSE of fit.
-        feature_list.append(utils.power_law_fit(edge_betweenness_centrality,bins=bins)[0][-2])# value 'a' in power law
-        feature_list.append(utils.power_law_fit(edge_betweenness_centrality,bins=bins)[1])# value sse in power law
+        for i in range(len(bins)):
+            # Adding to featurenames
+            feature_names.append('opt_model_{}'.format(bins[i]))
+            feature_names.append('powerlaw_a_{}'.format(bins[i]))
+            feature_names.append('powerlaw_SSE_{}'.format(bins[i]))
+            
+            # Fitting the edge betweenness centrality distribution and finding the optimal
+            # distribution according to SSE
+            opt_mod,opt_mod_sse = utils.best_fit_distribution(edge_betweenness_centrality,bins=bins[i])
+            feature_list.append(opt_mod)
+
+            # Fitting power law and finding 'a' and the SSE of fit.
+            feature_list.append(utils.power_law_fit(edge_betweenness_centrality,bins=bins[i])[0][-2]) # value 'a' in power law
+            feature_list.append(utils.power_law_fit(edge_betweenness_centrality,bins=bins[i])[1]) # value sse in power law
 
         # Fitting normal distribution and finding...
 
