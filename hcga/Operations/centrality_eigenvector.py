efdffa3f04d53b542c05684745887859b6aa3688
Henry Palasciano <Henry@dyn3131-31.wlan.ic.ac.uk>
2019-07-19 14:44:07 +0100

    moved bins and made changes to eigenvector and katz centrality

--- hcga/Operations/centrality_eigenvector.py
+++ hcga/Operations/centrality_eigenvector.py
@@ -24,7 +24,7 @@ class EigenCentrality():
         self.feature_names = []
         self.features = []
 
-    def feature_extraction(self,args):
+    def feature_extraction(self):
 
         """Compute the eigenvector centrality for the graph `G`.
 
@@ -79,10 +79,10 @@ class EigenCentrality():
         """
 
         # Defining the input arguments
-        bins = args[0]
+        bins = [10,20,50]
 
         # Defining featurenames
-        self.feature_names = ['mean','std','opt_model','powerlaw_a','powerlaw_SSE']
+        feature_names = ['mean','std','max','min']
 
         #G = self.G
         #op_obj = self.op_obj
@@ -104,15 +104,23 @@ class EigenCentrality():
         # Basic stats regarding the degree centrality distribution
         feature_list.append(eigenvector_centrality.mean())
         feature_list.append(eigenvector_centrality.std())
+        feature_list.append(eigenvector_centrality.max())
+        feature_list.append(eigenvector_centrality.min())
 
-        # Fitting the degree centrality distribution and finding the optimal
-        # distribution according to SSE
-        opt_mod,opt_mod_sse = utils.best_fit_distribution(eigenvector_centrality,bins=bins)
-        feature_list.append(opt_mod)
+        for i in range(len(bins)):
+            # Adding to featurenames
+            feature_names.append('opt_model_{}'.format(bins[i]))
+            feature_names.append('powerlaw_a_{}'.format(bins[i]))
+            feature_names.append('powerlaw_SSE_{}'.format(bins[i]))
+            
+            # Fitting the eigenvector centrality distribution and finding the optimal
+            # distribution according to SSE
+            opt_mod,opt_mod_sse = utils.best_fit_distribution(eigenvector_centrality,bins=bins[i])
+            feature_list.append(opt_mod)
 
-        # Fitting power law and finding 'a' and the SSE of fit.
-        feature_list.append(utils.power_law_fit(eigenvector_centrality,bins=bins)[0][-2]) # value 'a' in power law
-        feature_list.append(utils.power_law_fit(eigenvector_centrality,bins=bins)[1]) # value sse in power law
+            # Fitting power law and finding 'a' and the SSE of fit.
+            feature_list.append(utils.power_law_fit(eigenvector_centrality,bins=bins[i])[0][-2]) # value 'a' in power law
+            feature_list.append(utils.power_law_fit(eigenvector_centrality,bins=bins[i])[1]) # value sse in power law
 
         #Calculate the degree centrality of each node
 #        try:
@@ -144,4 +152,5 @@ class EigenCentrality():
         # Fitting exponential and finding ...
 
 
+        self.feature_names=feature_names
         self.features = feature_list
--- hcga/Operations/centrality_katz.py
+++ hcga/Operations/centrality_katz.py
@@ -12,7 +12,7 @@ class KatzCentrality():
         self.feature_names = []
         self.features = []
         
-    def feature_extraction(self,args):
+    def feature_extraction(self):
         """
         Compute Katz centrality for each node
         
@@ -39,9 +39,9 @@ class KatzCentrality():
             https://networkx.github.io/documentation/stable/reference/algorithms/centrality.html        """
         
         # Defining the input arguments
-        bins = args[0]
+        bins = [10,20,50]
         # Defining featurenames
-        feature_names = ['mean','std','opt_model','powerlaw_a','powerlaw_SSE']
+        feature_names = ['mean','std','max','min']
         G = self.G
         feature_list = []
         #Calculate the Katz centrality of each node
@@ -49,15 +49,23 @@ class KatzCentrality():
         # Basic stats regarding the Katz centrality distribution
         feature_list.append(katz_centrality.mean())
         feature_list.append(katz_centrality.std())
+        feature_list.append(katz_centrality.max())
+        feature_list.append(katz_centrality.min())
         
-        # Fitting the Katz centrality distribution and finding the optimal
-        # distribution according to SSE
-        opt_mod,opt_mod_sse = utils.best_fit_distribution(katz_centrality,bins=bins)
-        feature_list.append(opt_mod)
+        for i in range(len(bins)):
+            # Adding to featurenames
+            feature_names.append('opt_model_{}'.format(bins[i]))
+            feature_names.append('powerlaw_a_{}'.format(bins[i]))
+            feature_names.append('powerlaw_SSE_{}'.format(bins[i]))
+            
+            # Fitting the Katz centrality distribution and finding the optimal
+            # distribution according to SSE
+            opt_mod,opt_mod_sse = utils.best_fit_distribution(katz_centrality,bins=bins[i])
+            feature_list.append(opt_mod)
 
-        # Fitting power law and finding 'a' and the SSE of fit.
-        feature_list.append(utils.power_law_fit(katz_centrality,bins=bins)[0][-2])# value 'a' in power law
-        feature_list.append(utils.power_law_fit(katz_centrality,bins=bins)[1])# value sse in power law
+            # Fitting power law and finding 'a' and the SSE of fit.
+            feature_list.append(utils.power_law_fit(katz_centrality,bins=bins[i])[0][-2]) # value 'a' in power law
+            feature_list.append(utils.power_law_fit(katz_centrality,bins=bins[i])[1]) # value sse in power law
 
         
         self.feature_names=feature_names
