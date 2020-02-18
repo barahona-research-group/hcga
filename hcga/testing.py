d3944dc9ef45c54fb087fda7cb297aa04a8a8187
Nathan Bernier <nathan.bernier@alumni.epfl.ch>
2020-02-18 12:41:37 +0100

    New tests for feature normalisation and graph classification

--- hcga/testing.py
+++ hcga/testing.py
@@ -9,18 +9,42 @@ import hcga.graphs as hcga_graphs
 
 class TestingGraphMethods(unittest.TestCase):
 
+    def setUp(self):
+        rgraphs, rlabels = synthetic_data_sbm(N=4, seed=42)
+        self.g = hcga_graphs.Graphs(graphs=rgraphs, graph_class=rlabels)
+    
     def test_calculate_features(self):
         """
         Testing of the method calculate_features from the class Graphs
         of hcga.graphs with synthetic data
         """
         hashsum_ref = -505139337749376602
-        rgraphs, rlabels = synthetic_data_sbm(N=4, seed=42)
-        g = hcga_graphs.Graphs(graphs=rgraphs, graph_class=rlabels)
-        g.n_processes = 4
-        g.calculate_features(calc_speed='veryfast', parallel = True)
-        hashsum = pd.util.hash_pandas_object(g.graph_feature_matrix).sum()
+        
+        self.g.n_processes = 4
+        self.g.calculate_features(calc_speed='veryfast', parallel = True)
+        hashsum = pd.util.hash_pandas_object(self.g.graph_feature_matrix).sum()
         self.assertEqual(hashsum, hashsum_ref)
+        
+    def test_normalise_feature_data(self):
+        np.random.seed(42)
+        hashsum_ref = 8994831284871540922
+        feature_matrix = pd.DataFrame(np.random.uniform(1,100,size=(4, 400)))
+        self.g.graph_feature_matrix = feature_matrix
+        self.g.normalise_feature_data()
+        hashsum = pd.util.hash_pandas_object(pd.DataFrame(self.g.X_norm)).sum()
+        self.assertEqual(hashsum, hashsum_ref)
+        
+    def test_graph_classification(self):
+        final_acc_ref = 0.7
+        np.random.seed(42)
+        hashsum_ref = -6561133957446616237
+        n_graphs = 10
+        feature_matrix = pd.DataFrame(np.random.uniform(1,100,size=(n_graphs, 400)))
+        labels = [1]*(3) + [2]*(n_graphs-3)
+        self.g.graph_feature_matrix = feature_matrix
+        self.g.graph_labels = labels
+        final_acc = self.g.graph_classification(reduc_threshold = 0.5, plot=False) 
+        self.assertEqual(final_acc, final_acc_ref)
 
 
 def synthetic_data_sbm(N=1000, seed=None):
