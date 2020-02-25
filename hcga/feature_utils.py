247fee1a5bc6443e356d428d98a67fa723751f99
Nathan Bernier <nathan.bernier@alumni.epfl.ch>
2020-02-25 15:26:21 +0100

    Implemented interpretability score

--- hcga/feature_utils.py
+++ hcga/feature_utils.py
@@ -2,65 +2,75 @@
 import scipy.stats as st
 import numpy as np
 
+#np.seterr(all='raise')
 
 def normalize_features(feature_instance):
     """triple the number of features by normalising by node and edges"""
 
+    interpretability_downgrade = 1
     for feature_name in list(feature_instance.features.keys()):
+        feat_interpret = feature_instance.get_feature_interpretability(feature_name)
+        feat_desc = feature_instance.get_feature_description(feature_name)
         feature_instance.add_feature(
                 feature_name + "_N", 
                 feature_instance.features[feature_name] / len(feature_instance.graph.nodes),
-                (feature_instance.__class__.feature_descriptions[feature_name] 
-                    + ", normalised by the number of nodes in the graph"),
+                feat_desc + ", normalised by the number of nodes in the graph", 
+                feat_interpret - interpretability_downgrade,
                 )
         feature_instance.add_feature(
                 feature_name + "_E", 
                 feature_instance.features[feature_name] / len(feature_instance.graph.edges),
-                (feature_instance.__class__.feature_descriptions[feature_name] 
-                    + ", normalised by the number of edges in the graph"),
+                feat_desc + ", normalised by the number of edges in the graph",
+                feat_interpret - interpretability_downgrade,
                 )
 
-def summary_statistics(add_feature, feat_dist, feat_name, feat_desc):
+def summary_statistics(add_feature, feat_dist, feat_name, feat_desc, feat_interpret):
     """Computes summary statistics of distributions"""
     compl_desc = " of the distribution of " + feat_desc
 
-    add_feature(feat_name + "_mean", np.mean(feat_dist), "Mean" + compl_desc)
-    add_feature(feat_name + "_min", np.min(feat_dist), "Minimum" + compl_desc)
-    add_feature(feat_name + "_max", np.max(feat_dist), "Maximum" + compl_desc)
-    add_feature(feat_name + "_median", np.median(feat_dist), "Median" + compl_desc)
-    add_feature(feat_name + "_std", np.std(feat_dist), "Standard deviation" + compl_desc)
-    add_feature(feat_name + "_gmean", st.gmean(feat_dist), "G. Mean" + compl_desc)
+    add_feature(feat_name + "_mean", np.mean(feat_dist), 
+            "Mean" + compl_desc, feat_interpret - 0)
+    add_feature(feat_name + "_min", np.min(feat_dist), 
+            "Minimum" + compl_desc, feat_interpret - 0)
+    add_feature(feat_name + "_max", np.max(feat_dist), 
+            "Maximum" + compl_desc, feat_interpret - 0)
+    add_feature(feat_name + "_median", np.median(feat_dist), 
+            "Median" + compl_desc, feat_interpret - 0)
+    add_feature(feat_name + "_std", np.std(feat_dist), 
+            "Standard deviation" + compl_desc, feat_interpret - 0)
+    add_feature(feat_name + "_gmean", st.gmean(feat_dist), 
+            "G. Mean" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_hmean", st.hmean(np.abs(feat_dist) + 1e-8),
-        "G. Mean" + compl_desc)
+        "G. Mean" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_kurtosis", st.kurtosis(feat_dist),
-        "Kurtosis" + compl_desc)
+        "Kurtosis" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_mode", st.mode(feat_dist)[0][0],
-        "Mode" + compl_desc)
+        "Mode" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_kstat", st.kstat(feat_dist),
-        "Kstat" + compl_desc)
+        "Kstat" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_kstatvar", st.kstatvar(feat_dist),
-        "Kstat variance" + compl_desc)
+        "Kstat variance" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_tmean", st.tmean(feat_dist),
-        "Tmean" + compl_desc)
+        "Tmean" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_tmean", st.tvar(feat_dist),
-        "Tvariance" + compl_desc)
+        "Tvariance" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_tmin", st.tmin(feat_dist),
-        "Tminimum" + compl_desc)
+        "Tminimum" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_tmax", st.tmax(feat_dist),
-        "Tmaximum" + compl_desc)
+        "Tmaximum" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_tstd", st.tstd(feat_dist),
-        "T standard deviation" + compl_desc)
+        "T standard deviation" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_tsem", st.tsem(feat_dist),
-        "T sem" + compl_desc)
+        "T sem" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_variation", st.variation(feat_dist),
-        "Variation" + compl_desc)
+        "Variation" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_mean_repeats", np.mean(st.find_repeats(feat_dist)[1]),
-        "Mean repeats" + compl_desc)
+        "Mean repeats" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_entropy", st.entropy(feat_dist),
-        "Entropy" + compl_desc)
+        "Entropy" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_sem", st.sem(feat_dist),
-        "SEM" + compl_desc)
+        "SEM" + compl_desc, feat_interpret - 1)
     add_feature(feat_name + "_bayes_confint", 
         st.bayes_mvs(feat_dist)[0][1][1] - st.bayes_mvs(feat_dist)[0][1][0],
-        "Bayes confidance interval" + compl_desc)
+        "Bayes confidance interval" + compl_desc, feat_interpret - 1)
 
--- hcga/features/feature_class.py
+++ hcga/features/feature_class.py
@@ -32,30 +32,55 @@ class FeatureClass():
                 'keywords': self.__class__.keywords
                 }
 
-    def get_feature_info(self, f_name):
-        """Returns a dictionary of information about the feature f_name"""
+    def _test_feature_exists(self, f_name):
+        """Test if feature f_name exists in description list"""
         if f_name not in  self.__class__.feature_descriptions:
             raise Exception('Feature {} does not exist in class {}'.format(
                 f_name, self.name))
-        feat_dict = self.get_info()
-        feat_dict['feature_name'] = f_name
-        feat_dict['feature_description'] = self.__class__.feature_descriptions[f_name]
-        return feat_dict
+
+    def get_feature_info(self, f_name):
+        """Returns a dictionary of information about the feature f_name"""
+        self._test_feature_exists(f_name)
+        feat_info = self.get_info()
+        feat_info['feature_name'] = f_name
+        feat_dict = self.__class__.feature_descriptions[f_name]
+        feat_info['feature_description'] = feat_dict['desc']
+        feat_info['feature_interpretability'] = feat_dict['interpret']
+        return feat_info
+
+    def get_feature_description(self, f_name):
+        """Returns interpretability score of the feature f_name"""
+        self._test_feature_exists(f_name)
+        feat_dict = self.__class__.feature_descriptions[f_name]
+        return feat_dict['desc']
+
+    def get_feature_interpretability(self, f_name):
+        """Returns interpretability score of the feature f_name"""
+        self._test_feature_exists(f_name)
+        feat_dict = self.__class__.feature_descriptions[f_name]
+        return feat_dict['interpret']
 
     @classmethod
-    def add_feature_description(cls, f_name, f_desc):
+    def add_feature_description(cls, f_name, f_desc, f_interpret):
         """Adds the description to the class variable if not already there"""
         if f_name not in cls.feature_descriptions:
-            cls.feature_descriptions[f_name] = f_desc
+            cls.feature_descriptions[f_name] = {
+                    'desc':f_desc, 'interpret':f_interpret}
 
-    def add_feature(self, feature_name, feature_value, feature_description):
+    def add_feature(self, feature_name, feature_value, 
+            feature_description, feature_interpret):
         """Adds a computed feature value and its description"""
+        if not hasattr(feature_interpret, 'get_score'):
+            feature_interpret = InterpretabilityScore(feature_interpret)
         self.features[feature_name] = feature_value
-        self.__class__.add_feature_description(feature_name, feature_description)
+        self.__class__.add_feature_description(
+                feature_name, feature_description, feature_interpret)
 
     def compute_features(self):
         """main feature extraction function"""
-        self.add_feature('test', 0., 'Test feature for the base feature class')
+        self.add_feature('test', 0., 
+                'Test feature for the base feature class', 5
+                )
 
     def update_features(self, all_features):
         """update the feature dictionary if correct mode provided"""
@@ -68,4 +93,55 @@ class FeatureClass():
         if self.__class__.normalize_features:
             normalize_features(self)
         all_features[self.__class__.shortname] = self.features
+
+class InterpretabilityScore:
+    min_score = 0
+    max_score = 5
+
+    def __init__(self, score):
+        """
+        Init function for InterpretabilityScore
+
+        Parameters
+        ----------
+        score: number or {'min', 'max'}
+            value of score to set, 
+            will be shrunk to be within [min_score, max_score]
+        """
+        if score == 'max':
+            score = self.max_score
+        elif score == 'min':
+            score = self.min_score
+        self.set_score(score)
+
+    def set_score(self, score):
+        score_int = int(score)
+        if score_int < self.min_score:
+            score_int = self.min_score
+        elif score_int > self.max_score:
+            score_int = self.max_score
+        self.score = score_int
+
+    def get_score(self):
+        return self.score
+
+    def __add__(self, other):
+        if type(other) == int:
+            othervalue = other
+        else: othervalue = other.get_score()
+        result = self.__class__(self.get_score() + othervalue)
+        return result
+
+    def __sub__(self, other):
+        if type(other) == int:
+            othervalue = other
+        else: othervalue = other.get_score()
+        result = self.__class__(self.get_score() - othervalue)
+        return result
+
+    def __str__(self):
+        return str(self.score)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '({})'.format(self.__str__())
 
