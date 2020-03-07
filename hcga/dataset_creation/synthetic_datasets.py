1602f4a4f295b43d952ee996f37d22d1e7070f8e
alexis arnaudon <alexis.arnaudon@epfl.ch>
2020-03-07 15:08:26 +0100

    update TESTDATA to use with analysis

--- hcga/dataset_creation/synthetic_datasets.py
+++ hcga/dataset_creation/synthetic_datasets.py
@@ -11,7 +11,6 @@ import wget
 from ..io import save_dataset
 
 
-
 def synthetic_data():
 
     graphs = [
--- hcga/dataset_creation/test_datasets.py
+++ hcga/dataset_creation/test_datasets.py
@@ -18,40 +18,44 @@ def add_dummy_node_features(graph):
     return graph
 
 
-def make(folder="./datasets", add_features=False, write_to_file=True):
+def make(folder="./datasets", add_features=False, write_to_file=True, n_graphs=5):
     """ Makes pickle with graphs that test robustness of hcga """
 
     graphs = []
 
     # one, two and three node graphs
-    graphs.append(_add_graph_desc(nx.grid_graph([1]), "one-node graph"))
-    graphs.append(_add_graph_desc(nx.grid_graph([2]), "two-node graph"))
-    graphs.append(_add_graph_desc(nx.grid_graph([3]), "three-node graph"))
+    for i in range(n_graphs):
+        graphs.append(_add_graph_desc(nx.grid_graph([1]), "one-node graph"))
+        graphs.append(_add_graph_desc(nx.grid_graph([2]), "two-node graph"))
+        graphs.append(_add_graph_desc(nx.grid_graph([3]), "three-node graph"))
 
     # no edges
     G = nx.Graph()
     G.add_node(0)
     G.add_node(1, weight=2)
     G.add_node(2, weight=3)
-    graphs.append(_add_graph_desc(G, "graph without edges"))
+    for i in range(n_graphs):
+        graphs.append(_add_graph_desc(G, "graph without edges"))
 
     # directed graph no weights
     G = nx.DiGraph()
     G.add_nodes_from(range(100, 110))
-    graphs.append(_add_graph_desc(G, "directed graph with no weights"))
+    for i in range(n_graphs):
+        graphs.append(_add_graph_desc(G, "directed graph with no weights"))
 
     # directed graph weighted
     G = nx.DiGraph()
     H = nx.path_graph(10)
     G.add_nodes_from(H)
     G.add_edges_from(H.edges)
-    graphs.append(_add_graph_desc(G, "directed graph weighted"))
+    for i in range(n_graphs):
+        graphs.append(_add_graph_desc(G, "directed graph weighted"))
 
     # adding features to all
     if add_features:
         graphs = [add_dummy_node_features(graph) for graph in graphs]
 
-    labels = np.arange(len(graphs))
+    labels = np.random.randint(0, 2, len(graphs))
 
     if write_to_file:
         save_dataset(graphs, labels, "TESTDATA", folder=folder)
